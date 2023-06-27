09852628718c4f32aaff4f5df726fcc122154ca7
Matthew D. Groves <me@mgroves.com>
2023-06-27 11:30:48 -0400

    #2 integration test (using Capella) for registration

--- Conduit/Conduit.Tests/Integration/Auth/Handlers/RegistrationRequestHandlerTests.cs
+++ Conduit/Conduit.Tests/Integration/Auth/Handlers/RegistrationRequestHandlerTests.cs
@@ -7,22 +7,20 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Conduit.Tests.Integration.Auth.Handlers;
 
+[TestFixture]
 public class RegistrationRequestHandlerTests : CouchbaseIntegrationTest
 {
-    private ServiceProvider _serviceProvider;
-
     [OneTimeSetUp]
     public override async Task Setup()
     {
         await base.Setup();
 
-        ServiceCollection.AddCouchbaseBucket<IConduitBucketProvider>("Conduit", b =>
+        ServiceCollection.AddCouchbaseBucket<IConduitBucketProvider>("ConduitIntegrationTests", b =>
         {
             b
                 .AddScope("_default")
                 .AddCollection<IConduitUsersCollectionProvider>("Users");
         });
-        _serviceProvider = ServiceCollection.BuildServiceProvider();
     }
 
     [Test]
@@ -30,27 +28,27 @@ public class RegistrationRequestHandlerTests : CouchbaseIntegrationTest
     {
         // *** arrange
         // arrange collection provider
-        var usersCollectionProvider = _serviceProvider.GetRequiredService<IConduitUsersCollectionProvider>();
-
+        var usersCollectionProvider = ServiceProvider.GetRequiredService<IConduitUsersCollectionProvider>();
+    
         // arrange the handler
         var authService = new AuthService();
         var registrationRequestHandler = new RegistrationRequestHandler(usersCollectionProvider, authService, new RegistrationRequestValidator(new SharedUserValidator<RegistrationUserSubmitModel>(), usersCollectionProvider));
-
+    
         // arrange the request
         var submittedInfo = new RegistrationUserSubmitModel
         {
             Username = $"validuser{Path.GetRandomFileName()}",
-            Password = $"{Path.GetRandomFileName()}{Guid.NewGuid()}",
+            Password = $"Uppercase1!{Path.GetRandomFileName()}{Guid.NewGuid()}",
             Email = $"matt{Path.GetRandomFileName()}@example.com"
         };
         var request = new RegistrationRequest(new RegistrationSubmitModel
         {
             User = submittedInfo
         });
-
+    
         // *** act
         var result = await registrationRequestHandler.Handle(request, CancellationToken.None);
-
+    
         // *** assert
         // assert that the correct result was returned
         Assert.That(result, Is.Not.Null);
--- Conduit/Conduit.Tests/Integration/CouchbaseIntegrationTest.cs
+++ Conduit/Conduit.Tests/Integration/CouchbaseIntegrationTest.cs
@@ -1,126 +1,84 @@
-﻿using System.Net.Http.Headers;
-using System.Text;
-using Conduit.Migrations;
+﻿using Conduit.Migrations;
 using Couchbase;
-using Couchbase.Diagnostics;
 using Couchbase.Extensions.DependencyInjection;
-using Couchbase.Management.Buckets;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NoSqlMigrator.Runner;
-using Testcontainers.Couchbase;
 
 namespace Conduit.Tests.Integration;
 
 public abstract class CouchbaseIntegrationTest
 {
-    protected CouchbaseContainer CouchbaseTestContainer;
     protected ServiceCollection ServiceCollection;
+    private IConfigurationRoot _config;
+    private IBucket _bucket;
+    private ICluster _cluster;
+    private ServiceProvider? _serviceProvider;
+
+    protected ServiceProvider ServiceProvider
+    {
+        get { return _serviceProvider ??= ServiceCollection.BuildServiceProvider(); }
+    }
+
 
     [OneTimeSetUp]
     public virtual async Task Setup()
     {
-        CouchbaseTestContainer = new CouchbaseBuilder()
-            .WithImage("couchbase/server:7.2.0")
+        _config = new ConfigurationBuilder()
+            .AddJsonFile("integrationtestsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
             .Build();
 
-        await CouchbaseTestContainer.StartAsync();
+        _cluster = await Cluster.ConnectAsync(
+            _config["Couchbase:ConnectionString"],
+            _config["Couchbase:Username"],
+            _config["Couchbase:Password"]);
 
-        var cluster = await Cluster.ConnectAsync(CouchbaseTestContainer.GetConnectionString(), "Administrator", "password");
+        var allBuckets = await _cluster.Buckets.GetAllBucketsAsync();
+        var doesBucketExist = allBuckets.Any(b => b.Key == _config["Couchbase:BucketName"]);
+        if (!doesBucketExist)
+            throw new Exception($"There is no bucket for integration testing named '{_config["Couchbase:BucketName"]}'.");
 
-        // don't need the built-in bucket
-        var existingBucketName = CouchbaseTestContainer.Buckets.First().Name;
-        var bucketManager = cluster.Buckets;
-        await bucketManager.DropBucketAsync(existingBucketName);
+        _bucket = await _cluster.BucketAsync(_config["Couchbase:BucketName"]);
 
-        // create "Conduit" bucket that works with Migrations
-        // TODO: get this bucket name from appsettings/environment variable
-        await bucketManager.CreateBucketAsync(new BucketSettings()
-        {
-            Name = "Conduit",
-            BucketType = BucketType.Couchbase,
-            NumReplicas = 0,
-            RamQuotaMB = 100
-        });
-
-        // make sure new bucket is ready
-        // var bucketProvider = ServiceProvider.GetRequiredService<IConduitBucketProvider>();
-        // var bucket = await bucketProvider.GetBucketAsync();
-        var bucket = await cluster.BucketAsync("Conduit");
-        await bucket.WaitUntilReadyAsync(TimeSpan.FromSeconds(30));
-
-        await WaitForQueryService(bucket);
-        await EnsureIndexEngineSet();
-
-        // since this is a brand new couchbase instance
-        // that only has a bucket and _default scope/collection
-        // run the migrations
+        // *** nosql migrations
         var runner = new MigrationRunner();
-        var settings = new RunSettings();
-        settings.Direction = DirectionEnum.Up;
-        settings.Bucket = bucket;
-        await runner.Run(typeof(CreateUserCollectionInDefaultScope).Assembly, settings);
+        // run the migrations down just to be safe
+        var downSettings = new RunSettings();
+        downSettings.Direction = DirectionEnum.Down;
+        downSettings.Bucket = _bucket;
+        await runner.Run(typeof(CreateUserCollectionInDefaultScope).Assembly, downSettings);
 
-        await cluster.DisposeAsync();
+        // run the migrations
+        var upSettings = new RunSettings();
+        upSettings.Direction = DirectionEnum.Up;
+        upSettings.Bucket = _bucket;
+        await runner.Run(typeof(CreateUserCollectionInDefaultScope).Assembly, upSettings);
 
-        await BuildServiceCollection();
-    }
-
-    private async Task BuildServiceCollection()
-    {
         // create a DI setup, in order to get Couchbase DI objects 
         var services = new ServiceCollection();
         services.AddLogging();
         services.AddCouchbase(options =>
         {
-            options.ConnectionString = CouchbaseTestContainer.GetConnectionString();
-            options.UserName = "Administrator";
-            options.Password = "password";
+            options.ConnectionString = _config["Couchbase:ConnectionString"];
+            options.UserName = _config["Couchbase:Username"];
+            options.Password = _config["Couchbase:Password"];
         });
         ServiceCollection = services;
     }
 
-    private async Task EnsureIndexEngineSet()
-    {
-        var httpClient = new HttpClient();
-        var url = $"http://{CouchbaseTestContainer.Hostname}:{CouchbaseTestContainer.GetMappedPublicPort(8091)}/settings/indexes";
-        var data = new FormUrlEncodedContent(new Dictionary<string, string>
-        {
-            { "storageMode", "plasma" }
-        });
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-            "Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes("Administrator:password")));
-        var response = await httpClient.PostAsync(url, data);
-        var responseBody = await response.Content.ReadAsStringAsync();
-    }
-
-    private async Task WaitForQueryService(IBucket bucket)
-    {
-        bool queryServiceReady = false;
-
-        do
-        {
-            // Use PingAsync to get the status of all services in the bucket
-            var report = await bucket.PingAsync();
-
-            var queryServices = report.Services
-                .Where(s => s.Key == "n1ql");
-
-            if (queryServices.Any())
-            {
-                var queryService = queryServices.First();
-                var endpoints = queryService.Value;
-                queryServiceReady = endpoints.Any(e => e.State == ServiceState.Ok);
-            }
-
-            // Wait for a while before checking again if services are not ready yet
-            if (!queryServiceReady)
-                Thread.Sleep(5000);
-        } while (!queryServiceReady);
-    }
-
     [OneTimeTearDown]
-    public async Task Teardown()
+    public async Task TearDown()
     {
-        await CouchbaseTestContainer.StopAsync();
+        // run the migrations down to clean up
+        var runner = new MigrationRunner();
+        var downSettings = new RunSettings();
+        downSettings.Direction = DirectionEnum.Down;
+        downSettings.Bucket = _bucket;
+        await runner.Run(typeof(CreateUserCollectionInDefaultScope).Assembly, downSettings);
+
+        // dispose couchbase services
+        await _cluster.DisposeAsync();
+        await ServiceProvider.GetRequiredService<ICouchbaseLifetimeService>().CloseAsync();
     }
 }
