18d041ffc9d6bd8f31c9164f6dbf7ff5a764106d
Matthew D. Groves <me@mgroves.com>
2023-06-30 10:40:39 -0400

    #6 changing migration to work with github workflow / secrets

--- Conduit/Conduit.Migrations/MigrateBase.cs
+++ Conduit/Conduit.Migrations/MigrateBase.cs
@@ -11,29 +11,38 @@ public abstract class MigrateBase : Migrate
 
     protected MigrateBase()
     {
+        var builder = new ConfigurationBuilder();
+
         // *** this feels SUPER hacky ***
         // the goal is to reuse the config from Conduit.Web
         // so that stuff like scope name and collection names can be used in this migrations
         // perhaps environment variables will be better suited in production?
         // ******************************
 
-        // get path that this assembly is in
-        string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-        UriBuilder uri = new UriBuilder(codeBase);
-        string path = Uri.UnescapeDataString(uri.Path);
-        var fullPathFolder = Path.GetDirectoryName(path);
+        try
+        {
+            // get path that this assembly is in
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var fullPathFolder = Path.GetDirectoryName(path);
 
-        // navigate back through folders to get to Conduit.Web
-        // this assumes that the Conduit.Migrations dll will always be
-        // in \Conduit\Conduit.Migrations\bin\Debug\net7.0
-        // (or \Conduit\Conduit.Migrations\bin\Release\net7.0)
-        var webFolder = Path.Combine(fullPathFolder, "..\\..\\..\\..\\Conduit.Web\\");
+            // navigate back through folders to get to Conduit.Web
+            // this assumes that the Conduit.Migrations dll will always be
+            // in \Conduit\Conduit.Migrations\bin\Debug\net7.0
+            // (or \Conduit\Conduit.Migrations\bin\Release\net7.0)
+            var webFolder = Path.Combine(fullPathFolder, "..\\..\\..\\..\\Conduit.Web\\");
 
-        // use appsettings and environment variables to build config
-        _config = new ConfigurationBuilder()
-            .SetBasePath(webFolder)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.Development.json", optional: true)
+            builder.SetBasePath(webFolder)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.Development.json", optional: true);
+        }
+        catch
+        {
+            // we're probably running on github workflow, so the above hack won't work
+        }
+
+        _config = builder
             .AddEnvironmentVariables()
             .Build();
     }
