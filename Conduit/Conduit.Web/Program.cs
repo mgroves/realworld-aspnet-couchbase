402ea2f75efcb622c56db365ba1d44738678180b
Matthew D. Groves <me@mgroves.com>
2023-07-07 14:53:17 -0400

    organized program.cs a little

--- Conduit/Conduit.Web/Program.cs
+++ Conduit/Conduit.Web/Program.cs
@@ -14,9 +14,6 @@ namespace Conduit.Web
     {
         public static void Main(string[] args)
         {
-            // TODO: refactor this stuff into extension methods / other classes
-            // TODO: because it's a lot to sort through!
-
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Configuration
@@ -24,11 +21,114 @@ namespace Conduit.Web
                 .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
+
+            builder.Services.AddConduitSwaggerSetup();
+
+            builder.Services.AddConduitAuthenticationSetup();
+
+            builder.Services.AddConduitServiceDependencies(builder.Configuration);
+
+            // ****************************************************
+
+            var app = builder.Build();
+
+            app.UseAuthentication();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+            // Add the following line to close the Couchbase connection inside the app.Run() method at the end of Program.cs
+            app.Lifetime.ApplicationStopped.Register(() =>
+            {
+                app.Services.GetRequiredService<ICouchbaseLifetimeService>().Close();
+            });
+        }
+    }
+
+    public static class ConduitServiceSetupExtension
+    {
+        /// <summary>
+        /// Services, validators, database
+        /// </summary>
+        public static void AddConduitServiceDependencies(this IServiceCollection @this, ConfigurationManager configManager)
+        {
+            @this.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+            @this.AddTransient(typeof(SharedUserValidator<>));
+            @this.AddTransient<IAuthService, AuthService>();
+            @this.AddTransient<IUserDataService, UserDataService>();
+            @this.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+            @this.AddCouchbase(configManager.GetSection("Couchbase"));
+            @this.AddCouchbaseBucket<IConduitBucketProvider>(configManager["Couchbase:BucketName"], b =>
+            {
+                b
+                    .AddScope(configManager["Couchbase:ScopeName"])
+                    .AddCollection<IConduitUsersCollectionProvider>(configManager["Couchbase:UsersCollectionName"]);
+            });
+        }
+    }
+
+    public static class AuthenticationSetupExtensions
+    {
+        /// <summary>
+        /// Add JWT authentication
+        /// </summary>
+        public static void AddConduitAuthenticationSetup(this IServiceCollection @this)
+        {
+            @this.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "ConduitAspNetCouchbase_Issuer", // Replace with your issuer
+                        ValidAudience = "ConduitAspNetCouchbase_Audience", // Replace with your audience
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6B{DqP5aT,3b&!YRgk29m@j$L7uvnxE")) // Replace with your secret key
+                    };
+                    options.Events = new JwtBearerEvents()
+                    {
+                        // this code strips out "Token" from the header
+                        // due to Conduit spec saying "Token" instead of "Bearer"
+                        OnMessageReceived = ctx =>
+                        {
+                            if (ctx.Request.Headers.ContainsKey("Authorization"))
+                            {
+                                var bearerToken = ctx.Request.Headers["Authorization"].ElementAt(0);
+                                var token = bearerToken.StartsWith("Token ") ? bearerToken.Substring(6) : bearerToken;
+                                ctx.Token = token;
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+        }
+    }
+
+    public static class SwaggerSetupExtensions
+    {
+        /// <summary>
+        /// Add OpenAI generation, setup swagger/swashbuckle
+        /// </summary>
+        public static void AddConduitSwaggerSetup(this IServiceCollection @this)
+        {
+            @this.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Conduit API", Version = "v1" });
 
@@ -64,77 +164,6 @@ namespace Conduit.Web
                         Array.Empty<string>()
                     }
                 });
-            });
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = "ConduitAspNetCouchbase_Issuer", // Replace with your issuer
-                        ValidAudience = "ConduitAspNetCouchbase_Audience", // Replace with your audience
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6B{DqP5aT,3b&!YRgk29m@j$L7uvnxE")) // Replace with your secret key
-                    };
-                    options.Events = new JwtBearerEvents()
-                    {
-                        // this code strips out "Token" from the header
-                        // due to Conduit spec saying "Token" instead of "Bearer"
-                        OnMessageReceived = ctx =>
-                        {
-                            if (ctx.Request.Headers.ContainsKey("Authorization"))
-                            {
-                                var bearerToken = ctx.Request.Headers["Authorization"].ElementAt(0);
-                                var token = bearerToken.StartsWith("Token ") ? bearerToken.Substring(6) : bearerToken;
-                                ctx.Token = token;
-                            }
-
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
-
-            builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
-            builder.Services.AddTransient(typeof(SharedUserValidator<>));
-            builder.Services.AddTransient<IAuthService, AuthService>();
-            builder.Services.AddTransient<IUserDataService, UserDataService>();
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
-            builder.Services.AddCouchbase(builder.Configuration.GetSection("Couchbase"));
-            builder.Services.AddCouchbaseBucket<IConduitBucketProvider>(builder.Configuration["Couchbase:BucketName"], b =>
-            {
-                b
-                    .AddScope(builder.Configuration["Couchbase:ScopeName"])
-                    .AddCollection<IConduitUsersCollectionProvider>(builder.Configuration["Couchbase:UsersCollectionName"]);
-            });
-
-            // ****************************************************
-
-            var app = builder.Build();
-
-            app.UseAuthentication();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
-
-            // Add the following line to close the Couchbase connection inside the app.Run() method at the end of Program.cs
-            app.Lifetime.ApplicationStopped.Register(() =>
-            {
-                app.Services.GetRequiredService<ICouchbaseLifetimeService>().Close();
             });
         }
     }
