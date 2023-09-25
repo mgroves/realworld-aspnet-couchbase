baf9899404e118ad9c47fbf1b8066d6bb9bf9930
Matthew D. Groves <me@mgroves.com>
2023-09-25 11:02:22 -0400

    #20 trying to get more info why a couple of tests are failing only in CI

--- Conduit/Conduit.Tests/TestHelpers/JsonTestHelpers.cs
+++ Conduit/Conduit.Tests/TestHelpers/JsonTestHelpers.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Diagnostics;
+using App.Metrics.Logging;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
 namespace Conduit.Tests.TestHelpers;
 
@@ -6,9 +9,21 @@ public static class JsonTestHelpers
 {
     public static T SubDoc<T>(this string @this, string subDocKey)
     {
-        var obj = JObject.Parse(@this);
-        if (!obj.ContainsKey(subDocKey))
-            throw new ArgumentException($"SubDoc '{subDocKey}' not found in JSON.");
-        return obj[subDocKey].ToObject<T>();
+        try
+        {
+            var obj = JObject.Parse(@this);
+            if (!obj.ContainsKey(subDocKey))
+                throw new ArgumentException($"SubDoc '{subDocKey}' not found in JSON.");
+            return obj[subDocKey].ToObject<T>();
+        }
+        catch
+        {   
+            Trace.WriteLine("TRACE There was a problem getting the subdoc of '{subDocKey}' from this JSON string: '{@this}");
+            Debug.Print($"DEBUG There was a problem getting the subdoc of '{subDocKey}' from this JSON string: '{@this}");
+            var logfactory = (ILoggerFactory)new LoggerFactory();
+            var logger = new Logger<Conduit.Web.Program>(logfactory);
+            logger.LogError($"TRACE There was a problem getting the subdoc of '{subDocKey}' from this JSON string: '{@this}");
+            throw;
+        }
     }
 }
